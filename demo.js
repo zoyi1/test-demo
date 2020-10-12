2d55ce460ac7090aa379b5dfbc7972aaaa0ffdfe
dengyan <811852219@qq.com>
2020-10-12 09:51:30 +0800

    添加master测试文件代码

--- demo.js
+++ demo.js
@@ -2,6 +2,6 @@ window.onload = function(){
     let box = document.getElementsByClassName("box")[0];
     box.addEventListener("click", () =>{
         alert("这是测试代码！！");
-        console.log("这是合并分支的测试部分！！！")
     })
+    console.log("这是合并分支的测试部分！！！")
 }
