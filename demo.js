e5f2e9f3a28e9a4ced7f402eaae86fc3b7a99571
dengyan <811852219@qq.com>
2020-10-12 09:45:15 +0800

    修改分支代码

--- demo.js
+++ demo.js
@@ -1,6 +1,7 @@
 window.onload = function(){
     let box = document.getElementsByClassName("box")[0];
     box.addEventListener("click", () =>{
-        alert("这是测试代码！！")
+        alert("这是测试代码！！");
+        console.log("这是合并分支的测试部分！！！")
     })
 }
